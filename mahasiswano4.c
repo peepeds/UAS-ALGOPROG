eed4b98ebda6ee899c1f04d09ad12c61b15f2527
peepeds <111060939+peepeds@users.noreply.github.com>
2023-02-04 11:02:13 +0700

    Update mahasiswano4.c

--- mahasiswano4.c
+++ mahasiswano4.c
@@ -1,139 +1,49 @@
+// include library
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
+// define pewarnaan
+#define F "\x1b[1;91m" // False / warna merah 
+#define G "\x1b[1;32m" // Green / warna hijau 
+#define W "\x1b[1;37m" // white / putih
+#define B "\x1b[1;33m" // Yellow
+#define Y "\x1b[1;34m" // Blue
+#define R "\x1b[0m" // Reset
+#define S "\x1b[2m" // Faint 
+
+// targeting OS , hal ini disebabkan karena OS saya adalah MAC OS 
+// untuk mencegah error karena perbedaan OS maka saya melakukan targeting OS 
+#ifdef _WIN32
+    #define CLS system("cls")
+#else
+    #define CLS system("clear")
+#endif
+
+// untuk menangkap data pada file 
 struct mahasiswa{
     char nama [255] , JK;
     int usia , waktu ; 
+}mhs[20] ;
 
-}mhs[12] ;
 
-void nulis() ; void baca();int menu();
-int main(){
-    menu();
-}
+void menu(); void search(); void baca();
+int sortnama(); int sortusia();int berhenti(); int set(int x);
 
-void nulis(){
-    FILE *fp ;
-   
-    int i = 0;
-    int x, y , X , Y , len;
-    const long int NIM = 2602063043; //  NIM saya adalah 2602063043 
-    long int dummy = NIM ; 
-    char namaFile[260];
-
-    if(dummy > 1000){
-        dummy%= 1000 ;
-    }
-    printf("%003ld\n",dummy);
-    
-    sprintf(namaFile, "psikotest%003ld.dat", dummy);
-    fp = fopen(namaFile,"a+");
-
-    while(i<12){
-        x = i + 1;
-        y = x;
-        struct  mahasiswa x ;
-        if(i>0){
-            printf("Tekan ENTER untuk melanjutkan program.\n");
-        }
-        getchar();
-        printf("Masukkan Nama : ");
-        scanf("%[^\n]",mhs[i].nama);
-        len = strlen(mhs[i].nama); 
-
-        for(X=0 ; X < len ; X++){
-            if(mhs[i].nama[X]>=97 &&mhs[i].nama[X]<=122){
-                mhs[i].nama[X] -= 32 ;
-            }
-        }
-        for(Y=0 ; Y< len ; Y++){
-            if(Y==0){
-                if(mhs[i].nama[Y]>= 97 && mhs[i].nama[Y]<= 122){
-                    mhs[i].nama[Y] -= 32 ;
-                }
-            } else if(mhs[i].nama[Y]==32){  
-                if(mhs[i].nama[Y+1]>=97 && mhs[i].nama[Y+1]<=122){
-                    mhs[i].nama[Y] -= 32 ;
-                    Y++;
-                } 
-                Y++;
-                
-            }else { 
-                if(mhs[i].nama[Y]>=65 && mhs[i].nama[Y] <= 90){
-                    mhs[i].nama[Y] += 32 ;
-                }
-            }
-        }
-       
-        getchar();
-     
-        do{
-            printf("Masukkan Jenis Kelamin (L/P) : ");
-            scanf("%c",&mhs[i].JK);
-          
-            if(mhs[i].JK=='p'){
-                mhs[i].JK= 'P' ;
-            } else if(mhs[i].JK=='l'){
-                mhs[i].JK= 'L' ;
-            }
-            getchar();
-        }while(mhs[i].JK !='P'&& mhs[i].JK!='L');
-   
-        
-       
-        do{
-            printf("Masukkan usia : ");
-            scanf("%d",&mhs[i].usia);
-            getchar();
-        }while(!(mhs[i].usia >= 25 && mhs[i].usia <= 35));
-      
-
-        do{
-            printf("Masukkan durasi pengerjaan: ");
-            scanf("%d",&mhs[i].waktu);
-        
-            if(mhs[i].waktu < 5){
-                mhs[i].waktu *= 60 ;
-            }
-            getchar();
-        }while(!(mhs[i].waktu >= 120 && mhs[i].waktu <= 240));
-       
-        
-        
-        
-
-        fwrite(&mhs[i],sizeof(struct mahasiswa),1,fp);
-  
-
-        printf("\n");
-
-        puts("------------------------------------");
-        printf("Nama          : %s \n",mhs[i].nama);
-        printf("Jenis Kelamin : %c \n",mhs[i].JK);
-        printf("Usia          : %d \n",mhs[i].usia);
-        printf("Durasi        : %d \n",mhs[i].waktu);
-        puts("------------------------------------");
-        printf("\n");
-        
-       i++;
-    } 
-    fclose(fp);
-    menu();
-}
 
 void baca(){
- 
     FILE *fp ; 
     fp = fopen("psikotest043.dat","r");
-    int i = 0 , x, y;
+    int i = 0-1 , x, y;
    
-
-    while(i<12){
+    while(!feof(fp)){
+        i++;
         x = i + 1;
         struct  mahasiswa x ;
         fread(&mhs[i],sizeof(struct mahasiswa),1,fp);
-       
-        
+        // hapus komen multi line dibawah ini untuk mengetahui keseluruhan data yang disimpan.
+        /*
         printf("\n");
         puts("------------------------------------");
         printf("Nama          :%s \n",mhs[i].nama);
@@ -142,42 +52,238 @@ void baca(){
         printf("Durasi        :%d \n",mhs[i].waktu);
         puts("------------------------------------");
         printf("\n");
+        sleep(1);
+        if(i==9){
+            set(3);
+        }
+        */
         
-        i++;
     }
     fclose(fp);
     menu();
 }
-int menu(){  
+int set(int x){ // buat sleep & clear screen 
+    sleep(x);
+    CLS ; 
+    return -1 ;
+}
+int main(){
+    #ifdef _WIN32
+        printf(Y"Program akan dijalankan sebagai OS WINDOWS\n");
+        set(1);
+    #else
+        printf(Y"Program akan dijalankan dengan sistem UNIX\n");
+        set(1);
+        CLS;
+    #endif
 
+    baca();
+    
+
+}
+
+
+
+void menu(){  
     int pilihan ; 
    
-  
-    printf("+----------------------------------+\n");
+    set(0);
+    printf(W"+----------------------------------+\n");
     printf("|               Menu               |\n");
     puts("+----------------------------------+");
-    printf("|1. Write files                    |\n");
-    printf("|2. Read files                     |\n");
-    printf("|3. Exit                           |\n");
-    printf("+----------------------------------+\n\n");
+    printf("|1. Search Nama                    |\n");
+    printf("|2. Sorting by Name                |\n"); 
+    printf("|3. Sorting by Age                 |\n");
+    printf("|4. Exit                           |\n");
+    printf(W"+----------------------------------+\n\n");
    
-
-    printf("Masukkan menu yang ingin dipilih: ");
+    printf(S"Masukkan menu yang ingin dipilih: "R);
 
     scanf("%d",&pilihan);
     printf("\n");
-
+    set(0);
     if(pilihan==1){
       
-        nulis();
+        search();
     } else if(pilihan==2){
        
-        baca();
+        sortnama();
     } else if(pilihan == 3) {
       
-       return 0 ;
-    }else if ( pilihan != 1 && pilihan != 2 && pilihan != 3) {
-        printf("Perintah tidak valid\n"); menu();
+       sortusia();
+    }else if(pilihan==4){
+        
+        berhenti();
+    }else if ( pilihan != 1 && pilihan != 2 && pilihan != 3 && pilihan !=4) {
+        printf(F"Perintah tidak valid!\nSilahkan masukan ulang\n"R); sleep(2); CLS ;menu();
     }
-    return -1 ;
+}
+void search(){
+
+    int i=0 -1 , nilai=0; 
+    int  x , len , j , spasi=0 ;
+    char dicari[255];
+    
+    getchar();
+    printf("Masukkan nama yang ingin dicari: ");
+    scanf("%[^\n]",dicari); getchar();
+    len = strlen(dicari); 
+    
+    for(j=1 ; j < len ; j++){
+        if(dicari[j]>= 65 && dicari[j]<= 90){
+            dicari[j] += 32 ;
+        } else if(dicari[j]==32){
+          
+            spasi = j ;
+            if(dicari[j+1]>= 65 && dicari[j+1]<= 90){
+                dicari[j+1] += 32 ;
+             
+            } else if (dicari[j+1] >=97 && dicari[j+1]<= 122){
+                dicari[j+1] -= 32 ;
+               
+            }
+            j++;
+        }
+    }
+    if(dicari[0] >=97 && dicari[0]<= 122){
+        dicari[0] -= 32 ;
+    }else if(spasi !=0){
+        if(dicari[spasi+1]>=97 && dicari[spasi+1]<=122){
+            dicari[spasi+1] -= 32 ;
+        }
+    }
+    
+
+   
+    while(i++<20-1){
+        x = i + 1;
+        
+        struct  mahasiswa x ;
+        if(strcmp(dicari,mhs[i].nama)==0){
+            nilai = 1;
+            printf(W"------------------------------------\n");
+            printf(G"Nama          :%s \n",mhs[i].nama);
+            printf(G"Jenis Kelamin :%c \n",mhs[i].JK);
+            printf(G"Usia          :%d \n",mhs[i].usia);
+            printf(G"Durasi        :%d \n",mhs[i].waktu);
+            printf(W"------------------------------------\n");
+            sleep(1);
+         
+            break ;
+        }
+       
+    }
+
+
+    if(nilai==0){
+        printf(F"Nama tidak ada.\n");
+        sleep(1);
+    
+    }
+    set(0);
+    menu();
+    
+   
+}
+int berhenti(){
+    set(0);
+    printf(B"Program akan diberhentikan setelah pesan ini.\n");
+    set(1);
+    return 0 ;
+}
+
+int sortnama(){
+    int i , j  ;
+
+    struct mahasiswa tmp ; 
+
+    char stmp [255] ;
+    
+    for(i=0 ; i < 20-1; i++){
+        for(j= i+1 ;j< 20 ; j++){
+            if(strcmp(mhs[i].nama,mhs[j].nama)>0){
+                tmp = mhs[i] ; 
+                mhs[i] = mhs[j];
+                mhs[j] = tmp ;
+            } else if(strcmp(mhs[i].nama,mhs[j].nama)==0){
+                if(mhs[i].usia>mhs[j].usia){
+                    tmp = mhs[i] ; 
+                    mhs[i] = mhs[j];
+                    mhs[j] = tmp ;
+                }
+            }
+        }
+    }
+   
+    i = 0 ;
+    int pilihan ; 
+    printf(G"Berhasil di sorting\n"R);
+    set(1);
+    i=0-1;
+    while(i++<20-1){
+        if(mhs[i].usia==0){
+            continue;
+        }
+        printf(Y"\n");
+        puts("------------------------------------");
+        printf("Nama          :%s \n",mhs[i].nama);
+        //printf("Jenis Kelamin :%c \n",mhs[i].JK);
+        printf("Usia          :%d \n",mhs[i].usia);
+        //printf("Durasi        :%d \n",mhs[i].waktu);
+        puts("------------------------------------");
+        printf(R"\n");
+        
+    }
+    set(4);
+    menu();
+    return 0;
+}
+
+int sortusia(){
+    int i , j  ;
+
+    struct mahasiswa tmp ; 
+
+    char stmp [255] ;
+    
+    for(i=0 ; i < 12-1; i++){
+        for(j= i+1 ;j< 12 ; j++){
+            if(mhs[i].usia>mhs[j].usia){
+                tmp = mhs[i] ; 
+                mhs[i] = mhs[j];
+                mhs[j] = tmp ;
+            } else if(mhs[i].usia == mhs[j].usia){
+                if(strcmp(mhs[i].nama,mhs[j].nama)>0){
+                    tmp = mhs[i] ; 
+                    mhs[i] = mhs[j];
+                    mhs[j] = tmp ;
+                }
+            }
+        }
+    }
+   
+    i = 0-1 ;
+
+    int pilihan ; 
+    printf(G"Berhasil di sorting\n"R);
+    set(1);
+    
+    while(i++<20-1){
+        if(mhs[i].usia==0){
+            continue;
+        }
+       
+        printf(S"\n");
+        puts("------------------------------------");
+        printf("Nama          :%s \n",mhs[i].nama);
+        //printf("Jenis Kelamin :%c \n",mhs[i].JK);
+        printf("Usia          :%d \n",mhs[i].usia);
+        //printf("Durasi        :%d \n",mhs[i].waktu);
+        puts("------------------------------------");
+        printf(R"\n");
+
+    }
+    set(4);
+    menu();
+    return 0 ;
 }
